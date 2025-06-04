8b5dc1d6013c6fd5a36b35dde672104f8ad4ea78
Amini-su <aminitst@gmail.com>
2025-06-04 08:44:44 +0330

    Set up local Three.js dependencies

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
 // Scene setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
